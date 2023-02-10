4d7b056055b19c1a646a9205ad95a2c2f02cc97b
Patrick Chidiebere <okaforpatrick302@gmail.com>
2023-02-10 11:51:47 +0100

    Implemented collapse feature in projects ui

--- src/Components/UserAccount/UserAccount.js
+++ src/Components/UserAccount/UserAccount.js
@@ -1,25 +1,19 @@
 import React,{useState,useEffect,useRef} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./UserAccount.module.scss";
-import {
-  FaSun,
-  FaRegCalendarCheck,
-  FaRegCalendarAlt,
-} from "react-icons/fa";
 import {BiCheckCircle } from "react-icons/bi";
-import { IoIosSettings, IoIosSearch } from "react-icons/io";
-import { MdEventNote, MdOutlineWbTwilight } from "react-icons/md";
+import { IoIosArrowForward } from "react-icons/io";
 import { useDispatch,useSelector } from "react-redux";
 import { IoIosAdd,IoIosPricetag,IoIosClose } from "react-icons/io";
 import { ImFolderPlus } from "react-icons/im";
 import { useAuthState } from "react-firebase-hooks/auth";
 import {auth} from "../Firebase/Firebase"
 // import { async } from "@firebase/util";
-import { db } from "../Firebase/Firebase";
 // import {doc,getDoc} from "firebase/firestore"
 import { HiChevronLeft } from "react-icons/hi";
 import { IoMdFolderOpen } from "react-icons/io";
 import { ImUser } from "react-icons/im";
+import { IoIosArrowDown,IoIosArrowUp } from "react-icons/io";
 // FcList
 // FcTodoList
 // FcBullish
@@ -42,6 +36,11 @@ import {
   updateNumbersArray,
   setTasksTimesArray
 } from "../Settings/SettingsSlice";
+import { IoIosCheckmark } from "react-icons/io";
+import { 
+        ImFilesEmpt,
+        ImFolder,
+      } from "react-icons/im";
 // import {persistor} from "../Store/Store"
 // import { FetchTasks } from "../Settings/SettingsSlice";
 
@@ -60,6 +59,7 @@ const UserAccountUI = (props) => {
   const numbersArray = useSelector(state => state.settings.numbersArray)
   const avatarURL = useSelector(state => state.settings.userAvatarURL)
   const userBioData = useSelector(state => state.settings.userBioData)
+  const [collapseProjectContainer,setCollapseProjectContainer] = useState(false)
   console.log(userTasks);
   /////////Get projects /////////////////
   console.log(projects);
@@ -115,12 +115,10 @@ const UserAccountUI = (props) => {
     
     
     const handleClicked =  (projects,projectId) => {
-      //1.Get selected project from projects object and dispatch add task action
       selectProject(projects,projectId)
-     
-      //2. Get selected project's task object
-      //3. Add task to task store
-      console.log("clicked");
+    }
+    const handleProjectsCollapse = () => {
+      setCollapseProjectContainer(!collapseProjectContainer)
     }
   return (
     <div className={styles.UserAccountUI}>
@@ -171,31 +169,11 @@ const UserAccountUI = (props) => {
           <Link className={styles.link} to="/completedTasks">
             <BiCheckCircle className={styles.completedIcon} />
             <span className={styles.todayTodo}>Completed Tasks</span>
+            <IoIosArrowForward className={styles.arrowForward}/>
           </Link>
-          
         </div>
-        
-          
       </div>
-      <div className={styles.projects}>
-          {
-
-          projects && projects.map((project,i) => {
-           return <div  className={styles.project} ref = {linkRef} onClick = {() => handleClicked(projects,i)} key = {i}>
-              <div className={styles.projectWrapper}>
-                <div className={styles.colorAndProjectWrapper}>
-                  <span style={{backgroundColor: `${project.projectColor}`}} className={styles.projectColor}></span>
-                  <p className={styles.projectName}>{project.projectTitle}</p>
-                </div>
-                  {/* <div>
-                    <span className={styles.focusTime}>45m</span>
-                    <span className={styles.numberOfTask}>3</span>
-                  </div> */}
-              </div>
-            </div>
-          })
-        }
-        </div>
+      
         <div className={styles.addProjectWrapper}>
           <Link className={styles.linkToAddProject} to="/AddProject">
              <div className={styles.AddProject}>
@@ -210,11 +188,38 @@ const UserAccountUI = (props) => {
               </div>
           </Link>
         </div>
-        {/* <div style={{display: "flex",width: '100%', justifyContent: "space-around"}}>
-      {numbersArray.map((time,index) =>{
-        return <div onClick={() => removeNum(numbersArray,index)}>{time}</div>
-      })}
-     </div> */}
+        <div className={ styles.projectsFolderWrapper} onClick = {handleProjectsCollapse}>
+          <div className={styles.projectsListHeader}>
+              <ImFolder className= {styles.projectsFolderIcon}/>
+              <div className= {styles.projectsTitleContainer}>My Projects</div>
+              <div className={styles.dropDownIconContainer}>
+                {
+                 collapseProjectContainer ? < IoIosArrowUp className={[styles.projectsFolderIcon, styles.dropDownIcon].join(" ")}/>
+                 :
+                 <IoIosArrowDown className={[styles.projectsFolderIcon, styles.dropDownIcon].join(" ")}/>
+                }
+                
+              </div>
+              
+            </div>
+          <div className= {collapseProjectContainer ? styles.projectsListContainer : styles.collapseProjects}>
+            <div className={ styles.projects }>
+          {
+          projects && projects.map((project,i) => {
+           return <div  className={collapseProjectContainer ? styles.project : styles.collapseProjects} ref = {linkRef} onClick = {() => handleClicked(projects,i)} key = {i}>
+              <div className={styles.projectWrapper}>
+                <div className={styles.colorAndProjectWrapper}>
+                  <span style={{backgroundColor: `${project.projectColor}`}} className={styles.projectColor}></span>
+                  <p className={styles.projectName}>{project.projectTitle}</p>
+                </div>
+              </div>
+            </div>
+          })
+        }
+        </div>
+          </div>
+          
+        </div>
     </div>
   );
 };
